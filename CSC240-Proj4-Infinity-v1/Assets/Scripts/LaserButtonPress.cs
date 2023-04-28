f35464518782e3514e6d822f69eb26fbd216cfec
raohkenator <37935914+raohkenator@users.noreply.github.com>
2023-04-28 13:23:37 -0400

    Made the buttons function
    -Fixed collider box of button.
    -Made script to disable lasers on pushed.

--- CSC240-Proj4-Infinity-v1/Assets/Scripts/LaserButtonPress.cs
+++ CSC240-Proj4-Infinity-v1/Assets/Scripts/LaserButtonPress.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class LaserButtonPress : MonoBehaviour
 {
-    public GameObject ButtonBase;
+    public GameObject ButtonPushed;
     public GameObject Laser1;
-    public GameObject laser2;
+    public GameObject Laser2;
 
     // Start is called before the first frame update
     void Start()
     {
-        ButtonBase.SetActive(false);
+        ButtonPushed.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter()
+    {
+        Laser1.SetActive(false);
+        Laser2.SetActive(false);
+        gameObject.SetActive(false);
+        ButtonPushed.SetActive(true);
     }
 }
